6615ce1874dedea7aa1d911bfece8e5cd0018548
Samson <samsonbraimoh32@gmail.com>
2026-07-13 09:41:23 +0100

    setup for tenzies gamees done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import GameWrapper from './components/gameWrapper/gameWrapper'
+
+function App() {
+    return (
+    <>
+      <GameWrapper />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/gameWrapper/gameWrapper.jsx
@@ -0,0 +1,13 @@
+import style from './gameWrapper.module.css'
+
+export default function GameWrapper(){
+    return(
+        <>
+            <section className={style.main}>
+                <main className={style.gameContainer}>
+                    
+                </main>
+            </section>
+        </>
+    )
+}
